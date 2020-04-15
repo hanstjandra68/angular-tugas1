6b6955780876f8a890ed2cb2afee5bde6e9873f9
hanstjandra68 <c14180068@john.petra.ac.id>
2020-04-15 10:06:49 +0700

    1 file modified - done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,40 @@ export class AppComponent  {
   a=""
   b=""
   hasil = 0;
-  theHtmlString=""
-  polaB = "PolaB<br>***<br>****<br>*****<br>******<br>*******<br>********<br>";
-  polaA ="Pola A<br>*****o<br>****oo<br>***ooo<br>**<br>*<br>";
+  theHtmlString="";
+  polaB = "";
+  polaA ="";
   calculate(){
     this.hasil = Math.pow(parseInt(this.a), parseInt(this.b));
     if(this.hasil%2==0){
+       this.polaB=this.polaB.concat('Pola B <br>');
+       for (let i = 3; i <= this.hasil; i++) {
+          for(let j=0;j<i;j++){
+            this.polaB=this.polaB.concat('*');
+          }
+          this.polaB=this.polaB.concat('<br>');
+       }
        this.theHtmlString=this.polaB;
+       this.polaB="";
     }
     else{
-      this.theHtmlString=this.polaA;
+      this.polaA=this.polaA.concat('Pola A <br>');
+       for (let i = 0; i <= parseInt(this.b); i++) {
+          for(let j=parseInt(this.b);j>i;j--){
+            this.polaA=this.polaA.concat('*');
+          }
+          for(let k=0;k<=i;k++){
+            if(i<parseInt(this.a)){
+              this.polaA=this.polaA.concat('o');
+            }
+            else{
+              this.a="";
+            }
+          }
+          this.polaA=this.polaA.concat('<br>');
+       }
+       this.theHtmlString=this.polaA;
+       this.polaA="";
     }
   }
-  
 }
