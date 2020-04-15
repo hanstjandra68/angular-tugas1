1adc666f0b5cb9470c2f255654819388cef48b4c
hanstjandra68 <c14180068@john.petra.ac.id>
2020-04-15 09:25:15 +0700

    2 files modified - Commit done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  a=""
+  b=""
+  hasil = 0;
+  theHtmlString=""
+  polaB = "PolaB<br>***<br>****<br>*****<br>******<br>*******<br>********<br>";
+  polaA ="Pola A<br>*****o<br>****oo<br>***ooo<br>**<br>*<br>";
+  calculate(){
+    this.hasil = Math.pow(parseInt(this.a), parseInt(this.b));
+    if(this.hasil%2==0){
+       this.theHtmlString=this.polaB;
+    }
+    else{
+      this.theHtmlString=this.polaA;
+    }
+  }
+  
 }
